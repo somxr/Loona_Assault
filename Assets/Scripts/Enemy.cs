393f9a6ce0cc84a413e88f99c6ec215a0be183f5
Somar <somar.jaafar19@gmail.com>
2019-10-28 23:52:04 -0400

    106_Introducing_Encapsulation

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform spawnedAtRuntime;
+    [SerializeField] int scorePerHit = 10;
+    ScoreBoard scoreBoard;
+
+    void Start()
+    {
+        AddBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        Instantiate(deathFX, transform.position, Quaternion.identity, spawnedAtRuntime);
+        Destroy(gameObject);
+        scoreBoard.ScoreHit(scorePerHit);
+    }
+
+
+}
--- Assets/Scripts/SelfDestruct.cs
+++ Assets/Scripts/SelfDestruct.cs
@@ -2,22 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class SelfDestruct : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 5f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    void OnParticleCollision(GameObject other)
-    {
-        Destroy(gameObject);
     }
 }
